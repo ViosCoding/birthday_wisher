72db51bce76b62e71c39636f6dc4139d8fb7b29f
ViosCoding <138359680+ViosCoding@users.noreply.github.com>
2026-06-07 04:05:15 -0400

    main.py

--- main.py
+++ main.py
@@ -6,33 +6,56 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
+import os
 import pandas
 import random
 import smtplib
-import os
+import datetime as dt
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+email_closing = "Best regards,\nCorey London"
+LETTERS = []
+def choose_letter():
+    with open("letter_templates/letter_1.txt") as file:
+        letter_1 = file.read()
+    with open("letter_templates/letter_2.txt") as file:
+        letter_2 = file.read()
+    with open("letter_templates/letter_3.txt") as file:
+        letter_3 = file.read()
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+    LETTERS.append(letter_1)
+    LETTERS.append(letter_2)
+    LETTERS.append(letter_3)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+    generic_letter = random.choice(LETTERS)
+    letter_print = generic_letter.replace("[NAME]", recipient_name)
+    print(letter_print)
+
+
+    def send_letter():
+        """establishes the connection to mail server, starts tls security, establishes login credentials, then sends mail.
+         consider placing recipient as parameter to easily handle multiple"""
+        with smtplib.SMTP("smtp.gmail.com",
+                          port=587) as connection:  # connection to email server, must specify port number
+            connection.starttls()  # starts email protocol to stop interception
+            connection.login(user=my_email,
+                             password=app_password)  # login information for email, use app password instead of personal
+            connection.sendmail(from_addr=my_email, to_addrs=recipient_email,
+                                msg=f'Subject:Happy Birthday\n\n{letter_print} \n\n{email_closing}')
+    send_letter()
+
+
+now = dt.datetime.now()
+today = (now.month, now.day)
+data = pandas.read_csv("../birthdays.csv")
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+recipient_email = birthday_dict.get(today, {}).get('email')  # access a value after creating a dictionary using key names
+recipient_name = birthday_dict.get(today, {}).get('name')   # access a value after creating a dictionary using key names
+
+
+
+if today in birthday_dict:
+    choose_letter()
